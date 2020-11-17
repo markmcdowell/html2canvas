6bab623e91ca1305069e5915da118fa1d2120ca1
Mark McDowell <mark.mcdowell@reactivemarkets.com>
2020-11-17 12:31:06 +0000

    fix: transitions can be arrays
    Correcting duration parsing.

--- src/css/property-descriptors/duration.ts
+++ src/css/property-descriptors/duration.ts
@@ -1,9 +1,21 @@
-import {IPropertyTypeValueDescriptor, PropertyDescriptorParsingType} from '../IPropertyDescriptor';
+import {IPropertyListDescriptor, PropertyDescriptorParsingType} from '../IPropertyDescriptor';
+import {CSSValue, isDimensionToken} from '../syntax/parser';
 
-export const duration: IPropertyTypeValueDescriptor = {
+export const duration: IPropertyListDescriptor<number[]> = {
     name: 'duration',
     initialValue: '0s',
     prefix: false,
-    type: PropertyDescriptorParsingType.TYPE_VALUE,
-    format: 'duration'
+    type: PropertyDescriptorParsingType.LIST,
+    parse: (tokens: CSSValue[]): number[] => {
+        return tokens.filter(isDimensionToken).map(duration => {
+            switch (duration.unit.toLowerCase()) {
+                case 's':
+                    return 1000 * duration.number;
+                case 'ms':
+                    return duration.number;
+                default:
+                    return 0;
+            }
+        });
+    }
 };
